c029c95adc4f6081f366be8220c026b5df8b7f92
Zac Smith <zrs3141592@gmail.com>
2019-10-30 19:36:30 -0600

    install express and start index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+
+const express = require("express");
+
+// create a server
+const server = express();
+
+// middleware
+
+// route handlers
+
+// GET /api/users
+
+// GET /api/users/:id
+
+// POST /api/users
+
+// PUT /api/users/:id
+
+// DELETE /api/users/:id
+
+// server.listen
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log(`\n=== Server is listening on port ${PORT} ===\n`);
+});
